f8481022576ac0b5582c68aa7ddaa163da8e781d
JeanArce <113121403+JeanArce@users.noreply.github.com>
2023-05-07 12:33:15 +0200

    done in blog list and creating contact page

---
+++ scripts/blogs.js
@@ -0,0 +1,107 @@
+
+let currentPage = 1;
+let totalPosts = 0;
+let postsLists = [];
+
+
+const getPostsEndpoint = (currentPageVal) => {
+    const blogsListsEndpoint = `https://www.jeanarcenal.no/wp-json/wp/v2/posts?_embed&per_page=10&page=${currentPageVal}&orderby=date`;  
+    return blogsListsEndpoint;
+};
+
+
+const gettingPosts = async(endPointValue) => {
+    
+    const fetchData = await fetch(endPointValue, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    totalPosts = fetchData.headers.get('X-WP-Total');
+    const jsonData = await fetchData.json();
+
+
+    return jsonData;
+
+};
+
+
+const insertToPostLists = (data) => {
+
+
+    const blogListContainer = document.querySelector('.blogListContainer');
+
+    data.forEach(obj => {
+
+        postsLists.push(obj);
+        
+        const { id, title, _embedded, excerpt } = obj;
+
+          
+        const itemHtml = `
+
+            <div class="blogListItem">
+                <img src="${_embedded['wp:featuredmedia'][0].source_url}" alt="${_embedded['wp:featuredmedia'][0].alt_text}">
+                <h2>${title.rendered}</h2>
+                <div class="excerptContainer">
+                    ${excerpt.rendered}
+                </div>
+
+                <a href="/blog-details-page.html?id=${id}">View Details</a>
+            </div>
+        `;
+
+
+        blogListContainer.innerHTML += itemHtml
+    })
+
+  
+
+
+};
+
+const getPosts = async() => {
+
+    try {
+
+      
+
+
+        const postDatas = await gettingPosts(getPostsEndpoint(currentPage));
+
+        insertToPostLists(postDatas);
+        
+        const viewMoreBtn = document.querySelector('.viewMore');
+
+        if(postsLists.length && postsLists.length < totalPosts) {
+            viewMoreBtn.style.display = 'block';
+        }
+    
+        viewMoreBtn.addEventListener('click', async() => {
+            currentPage += 1;
+          
+
+            const postDatas = await gettingPosts(getPostsEndpoint(currentPage));
+
+            insertToPostLists(postDatas);
+
+            if(postsLists.length === Number(totalPosts)) {
+               
+                viewMoreBtn.style.display = 'none';
+            }
+
+        });
+
+
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+
+getPosts();
+
+
+
---
+++ scripts/details.js
@@ -0,0 +1,103 @@
+const queryString = document.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id');
+
+const detailsPostEndpoint = `https://www.jeanarcenal.no/wp-json/wp/v2/posts/${id}?_embed`;
+
+
+
+async function getPostDetails() {
+
+    try {
+        const fetchData = await fetch(detailsPostEndpoint, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        const jsonData = await fetchData.json();
+
+        console.log(jsonData);
+
+       const { title, _embedded, content } = jsonData;
+       console.log(title);
+       console.log(_embedded);
+       console.log(content);
+
+       const postDetailsContainer = document.getElementById("postDetails");
+       console.log(postDetailsContainer);
+
+       const itemDetails = `
+
+            <div class="topDetails">
+                <h1>${title.rendered}</h1>
+                <img src="${_embedded['wp:featuredmedia'][0].source_url}" alt="${_embedded['wp:featuredmedia'][0].alt_text}">
+            </div>
+
+            <div class="contentDetails">
+
+                ${content.rendered}
+            </div>
+       `;
+
+        postDetailsContainer.innerHTML = itemDetails;
+
+
+        
+
+        const modal = document.getElementById("myModal");
+        const closeBtn = modal.querySelector("#close");
+        const modalContent = modal.querySelector('.modal-content');
+        const modalImage =  modal.querySelector('#modalImage');
+
+       
+
+        document.addEventListener('click', (evt) => {
+            if (evt.target == modal) {
+                modalContent.style.opacity = 0;
+                modal.style.display = 'none';
+
+              
+            }
+        });
+
+        closeBtn.addEventListener('click', () => {
+            modalContent.style.opacity = 0;
+            modal.style.display = 'none';
+         
+        });
+
+
+        const images = postDetailsContainer.getElementsByTagName('img');
+
+       
+
+        for (let i = 0; i < images.length; i++) {
+            images[i].addEventListener('click', function() {
+                const selectedImg = images[i];
+               
+                modalImage.setAttribute('src', selectedImg.src);
+                modalImage.setAttribute('alt', selectedImg.alt);
+
+
+                modal.style.display = 'block';
+                setTimeout(() => {
+                    modalContent.style.opacity = 1;
+                }, 1000)
+                
+              
+            });
+        }
+
+
+
+
+    } catch(error) {
+        console.log(error);
+    }
+
+}
+
+
+getPostDetails();
