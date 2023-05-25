8a2c193d77df2dbcd1af1ea71ab52107586bda33
JeanArce <113121403+JeanArce@users.noreply.github.com>
2023-05-25 23:59:22 +0200

    adjusted

--- js/utility.js
+++ js/utility.js
@@ -1,20 +1,3 @@
-// export const renderItemsDetails = (product) => {
-    
-//     const {excerpt, _embedded, content} = product;
-    
-//     return `
-
-//         <div class="topDetails">
-//             <h1>${title.rendered}</h1>
-//             <img src="${_embedded['wp:featuredmedia'][0].source_url}" alt="${_embedded['wp:featuredmedia'][0].alt_text}">
-//         </div>
-
-//         <div class="contentDetails">
-
-//             ${content.rendered}
-//         </div>
-//     `;
-// };
 
 
 export const fetchingData = async(endpoint) => {
